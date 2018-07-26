6310cb8450144aaaa791be33911596c635d1f054
Nathan Knowler <nathan@knowler.me>
2018-07-26 11:44:30 -0600

    Re-add deploy info doc
    This adds a deploy/build info doc to each build so you have some way of
    identifying when it was built. It also gives a warning to any potential
    modifiers of the files, informing them of the correct process they
    should be following.

---
+++ bin/instructions.md
@@ -0,0 +1,8 @@
+- - -
+
+Developer: please commit your changes to the repository, build,
+prepare, and then distribute the changes. Do not manually upload
+individual file changes.
+
+Others: FIY there are no source assets in this directory. Please
+contact the developer if you want to make changes.
--- bin/prep.sh
+++ bin/prep.sh
@@ -23,6 +23,10 @@ cp -R {app,config,dist,resources,vendor} $temp  # Theme setup & config, views, t
 rm -rf $temp/resources/assets                   # Remove source assets
 cp LICENSE.md $temp                             # Copy Sage license (legally required)
 
+# Create build info and instructions file
+printf "Commit:   $hash\nBuilt at: $timestamp\nBuilt by: $(git config user.name)\n\n" > $temp/deploy-info.md
+cat bin/instructions.md >> $temp/deploy-info.md
+
 # Set distribute file name
 dist="$theme-distribute-$timestamp-$hash".zip
 
